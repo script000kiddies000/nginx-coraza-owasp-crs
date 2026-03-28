4f9722c47915d9af4ce5e109a96a8995256e3cfa
Your Name <scriptkiddies@protonmail.com>
2026-03-28 06:26:38 -0400

    feat: reason 403 error handling origin

--- dashboard/internal/nginx/generator.go
+++ dashboard/internal/nginx/generator.go
@@ -159,9 +159,10 @@ server {
         proxy_pass         http://{{upstreamName .Domain}};
         proxy_http_version 1.1;
         proxy_set_header   Connection      "";
-        proxy_set_header   Host            $http_host;
-        proxy_set_header   X-Real-IP       $remote_addr;
-        proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header   Host              $http_host;
+        proxy_set_header   X-Forwarded-Host  $http_host;
+        proxy_set_header   X-Real-IP         $remote_addr;
+        proxy_set_header   X-Forwarded-For   $proxy_add_x_forwarded_for;
         proxy_set_header   X-Forwarded-Proto $scheme;
         proxy_set_header   X-Request-ID    $request_id;
     }
